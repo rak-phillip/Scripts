72151d5dce1efec40c930afae9f7dc1002753d0a
Phillip Rak <prak@hoopercorp.com>
2014-02-04 14:31:40 -0600

    --Moved Main Script Logic to Function Moved all of the main script logic into function CopyAllAttachments()

--- copy_attachment.ps1
+++ copy_attachment.ps1
@@ -22,31 +22,36 @@ Function GetFolder([string]$folderPath, $session)
     return $folder    
 }
 
-Add-Type -assembly "Microsoft.Office.Interop.Outlook"
+Function CopyAllAttachments([string]$folderPath)
+{
+    Add-Type -assembly "Microsoft.Office.Interop.Outlook"
 
-$folderPath = "\\Public Folders\All Public Folders\Co:Training Records - (Unprocessed)"
-$session = New-Object -comobject Outlook.Application
+    $session = New-Object -comobject Outlook.Application
 
-$session.Version
+    $session.Version
 
-if (!($session.Version -like "12.*" -or $session.Version -like "14.*")){
-    write-host "Requires 2007 or 2010"
-    return
-}
+    if (!($session.Version -like "12.*" -or $session.Version -like "14.*")){
+        write-host "Requires 2007 or 2010"
+        return
+    }
 
-$trainingRecords = GetFolder $folderPath $session
+    $trainingRecords = GetFolder $folderPath $session
 
-$folderItems = $trainingRecords.Items
-$folderItems.Count
-$currentMail = $null
+    $folderItems = $trainingRecords.Items
+    $currentMail = $null
 
-foreach ($collectionitem in $folderItems) {
-    $currentMail = $collectionitem
-    if ($currentMail -ne $null) {
-        if ($currentMail.Attachments.Count -gt 0) {
-            for ($i = 1; $i -le $currentMail.Attachments.Count; $i++) {
-                $currentMail.Attachments.Item($i).SaveAsFile("D:\test\" + $currentMail.Attachments.Item($i).FileName);
+    foreach ($collectionitem in $folderItems) {
+        $currentMail = $collectionitem
+        if ($currentMail -ne $null) {
+            if ($currentMail.Attachments.Count -gt 0) {
+                for ($i = 1; $i -le $currentMail.Attachments.Count; $i++) {
+                    $currentMail.Attachments.Item($i).SaveAsFile("D:\test\" + $currentMail.Attachments.Item($i).FileName);
+                }
             }
         }
     }
 }
+
+$folderPath = "\\Public Folders\All Public Folders\Co:Training Records - (Unprocessed)"
+
+CopyAllAttachments $folderPath
