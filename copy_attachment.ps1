07cb3e3e3fff543b39b4622df83efc7fa0436bdb
Phillip Rak <prak@hoopercorp.com>
2014-02-04 14:20:54 -0600

    --Moved Folder Lookup to Function Moved the folder lookup to function GetFolder(), which follows the examples provided by Microsoft for the Outlook Object Model.

--- copy_attachment.ps1
+++ copy_attachment.ps1
@@ -1,53 +1,41 @@
+Function GetFolder([string]$folderPath, $session)
+{
+    $backslash = "\"
+    
+    if ($folderPath.StartsWith("\\")) {
+        $folderPath = $folderPath.Remove(0, 2)
+    }
+    
+    $folders = $folderPath.Split($backslash.ToCharArray())
+    $folder = $session.Session.Folders.item($folders[0])
+    
+    if ($folder -ne $null) {
+        for ($i = 1; $i -le $folders.GetUpperBound(0); $i++) {
+            $subFolders = $folder.Folders
+            $folder = $subFolders.item($folders[$i])
+            
+            if ($folder -eq $null) {
+                return $null
+            }            
+        }
+    }
+    return $folder    
+}
+
 Add-Type -assembly "Microsoft.Office.Interop.Outlook"
 
 $folderPath = "\\Public Folders\All Public Folders\Co:Training Records - (Unprocessed)"
 $session = New-Object -comobject Outlook.Application
 
 $session.Version
-$session.Session.Folders | ft Name
 
 if (!($session.Version -like "12.*" -or $session.Version -like "14.*")){
     write-host "Requires 2007 or 2010"
     return
 }
 
-$backslash = "\"
+$trainingRecords = GetFolder $folderPath $session
 
-if ($folderPath.StartsWith("\\")){
-    $folderPath = $folderPath.Remove(0, 2)
-}
-
-$folders = $folderPath.Split($backslash.ToCharArray())
-$folder = $session.Session.Folders
-$name = "Public Folders"
-
-foreach ($item in $folder) {
-    if ($item.Name -eq $name -or $item.DisplayName -eq $name) {
-        $publicFolder = $item
-    }
-}
-
-$publicFolder.Folders | ft Name
-$folder = $publicFolder.Folders
-$name = "All Public Folders"
-
-foreach ($item in $folder) {
-    if ($item.Name -eq $name -or $item.DisplayName -eq $name) {
-        $allPublicFolders = $item
-    }
-}
-
-$allPublicFolders.Folders | ft Name
-$folder = $allPublicFolders.Folders
-$name = "Co:Training Records - (Unprocessed)"
-
-foreach ($item in $folder) {
-    if ($item.Name -eq $name -or $item.DisplayName -eq $name) {
-        $trainingRecords = $item
-    }
-}
-
-$trainingRecords.Folders | ft Name
 $folderItems = $trainingRecords.Items
 $folderItems.Count
 $currentMail = $null
